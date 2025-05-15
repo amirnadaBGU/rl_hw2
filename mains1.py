97281bc396967a509bf59507da5eed37487fa0fd
ndvam <ndvamr@gmail.com>
2025-05-15 18:58:20 +0300

    first commit

--- mains1.py
+++ mains1.py
@@ -1,13 +1,15 @@
 import pyRDDLGym
 import numpy as np
-import matplotlib
-matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 
-# Paths to RDDL files
+# Global variables
+DEBUG = False
 base_path = './'
 domain_file = base_path + 'bandit_domain.rddl'
 instance_file = base_path + 'bandit_instance.rddl'
+
+# Parse horizon from instance file
+HORIZON = 20000 #TODO: horizon is not equal here and in instance file
 
 # Create the environment
 myEnv = pyRDDLGym.make(domain=domain_file, instance=instance_file)
@@ -19,10 +21,10 @@ arm_names = list(myEnv.action_space.keys())
 myEnv.reset()
 rewards = []
 
-for i in range(3):
+for i in range(HORIZON):
     # Choose a random arm
     chosen_arm = np.random.choice(arm_names)
-
+    chosen_arm = "roll___a1"
     # Create an action dict with all False, except one arm
     action = {arm: False for arm in arm_names}
     action[chosen_arm] = True
@@ -30,9 +32,10 @@ for i in range(3):
     # Step the environment
     next_state, reward, done, _, _ = myEnv.step(action)
 
-    print(f"Step {i+1}: Rolled {chosen_arm}, Reward: {reward}")
+    # print(f"Step {i+1}: Rolled {chosen_arm}, Reward: {reward}")
     rewards.append(reward)
 
+print(np.mean(rewards))
 # Optional: Plot the rewards
 plt.plot(rewards, marker='o')
 plt.xlabel('Step')
---
+++ string_creator.py
@@ -0,0 +1,9 @@
+N = 100
+arms = [f"a{i}" for i in range(1, N + 1)]
+
+print("arm : {" + ", ".join(arms) + "};")
+print("non-fluents {")
+print(f"    NUMBER_OF_ARMS = {N}.0;")
+for i, a in enumerate(arms, 1):
+    print(f"    ARM_NUM({a}) = {i}.0;")
+print("};")
