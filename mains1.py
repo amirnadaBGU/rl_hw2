8a6ffb1517d1abc180284e145dfd49dfafa36f34
ndvam <ndvamr@gmail.com>
2025-05-16 10:09:43 +0300

    first commit

--- mains1.py
+++ mains1.py
@@ -10,14 +10,73 @@ base_path = './'
 domain_file = base_path + 'bandit_domain.rddl'
 instance_file = base_path + 'bandit_instance.rddl'
 
-# Parse horizon from instance file
-HORIZON = 100 #TODO: horizon is not equal here and in instance file
-MEAN_BEST_REWARD_PER_ACTION = 0.5
+# GLOBALS
+HORIZON = 20000 # suppose to be 20000
+N_LOOPS = 1 # suppose to be 20
+MEAN_BEST_REWARD_PER_ACTION = 0.5 # for regret calculations
+
+def do_single_run_ucb1_policy():
+    pass
+
+def do_single_run_greedy_policy():
+    """
+    function that do full simulation with greedy policy according to instructions - Horizon arm choices sequence
+    ______
+    return:
+    ______
+        rewards - list of rewards. Shape: [r,r,r...] - length HORIZON
+    """
+    # Create the environment
+    myEnv = pyRDDLGym.make(domain=domain_file, instance=instance_file)
+    # Extract arm names from action space
+    arm_names = list(myEnv.action_space.keys())
+    # Start the environment
+    myEnv.reset()
+    # Construct reward poe arm array:
+    reward_per_arm = np.zeros(len(arm_names))
+    rewards = []
+    for i in range(len(arm_names)):
+        for j in range(100):
+            # Choose a arm in a sequntial order
+            chosen_arm = arm_names[i]
+
+            # Create an action dict with all False, except one arm
+            action = {arm: False for arm in arm_names}
+            action[chosen_arm] = True
+
+            # Step the environment
+            next_state, reward, done, _, _ = myEnv.step(action)
+
+            # Update reward per arm
+            reward_per_arm[i] += reward
+
+            # Update rewards
+            rewards.append(reward)
+    best_arm = np.argmax(reward_per_arm)
+    chosen_arm = arm_names[best_arm]
+
+    # Create an action dict with all False, except one arm
+    action = {arm: False for arm in arm_names}
+    action[chosen_arm] = True
+    for i in range(10000):
+
+        # Step the environment
+        next_state, reward, done, _, _ = myEnv.step(action)
+
+        # Update rewards
+        rewards.append(reward)
 
 
+    return rewards
 
-
-def random_policy():
+def do_single_run_random_policy():
+    """
+    function that do full simulation with random policy - Horizon arm choices sequence
+    ______
+    return:
+    ______
+        rewards - list of rewards. Shape: [r,r,r...] - length HORIZON
+    """
     # Create the environment
     myEnv = pyRDDLGym.make(domain=domain_file, instance=instance_file)
     # Extract arm names from action space
@@ -35,11 +94,18 @@ def random_policy():
         # Step the environment
         next_state, reward, done, _, _ = myEnv.step(action)
 
-        # print(f"Step {i+1}: Rolled {chosen_arm}, Reward: {reward}")
+        # Update rewards
         rewards.append(reward)
     return rewards
 
 def plot_rew_reg(rew_reg,headline):
+    """
+    function that plots a single graph of cumulative np array. can be rewards or regrest
+    ______
+    input:
+        rew_reg - average rewards/regret np array. Shaoe: [ar,ar,ar,....ar] - HORIZON
+        headline - string headline provided by the user
+    """
     cumulative_rew_reg = np.cumsum(rew_reg)
     avg_total_rew_reg = np.mean(rew_reg)
     print(f"Policy: {headline}, average quantity over {HORIZON} steps = {avg_total_rew_reg:.3f}")
@@ -51,27 +117,47 @@ def plot_rew_reg(rew_reg,headline):
     plt.show()
 
 def full_plot_run(policy):
+    """
+    function that calls 20 time input function policy
+    collects rewards, average them per step and plot them using plot_rew_reg
+
+    intput:
+        policy - function that runs a simulation with specific policy
+    """
     random_rewards_all_runs = []
     random_regrets_all_runs = []
-    for k in range(20):
-        # Collect rewards
+    for k in range(N_LOOPS):
+        print(f"Start {k+1} Run!")
+        # Collect rewards - Shape [r,r,r....] - length horizon
         rewards_per_run = policy()
 
-        # Collect regrets
+        # Collect regrets - Shape [r,r,r....] - length horizon
         regrets_per_run = MEAN_BEST_REWARD_PER_ACTION * np.ones(len(rewards_per_run))-rewards_per_run
 
+        # Summing - Shape [[r,r,r....],[r,r,r....]] - [[length horizon],] - length 20
         random_rewards_all_runs.append(rewards_per_run)
         random_regrets_all_runs.append(regrets_per_run)
-
+        print(f"Finish {k + 1} Run!")
+    # Shaing as numpy array from list
     random_rewards_all_runs = np.array(random_rewards_all_runs)
     random_regrets_all_runs = np.array(random_regrets_all_runs)
 
+    # Average value per step - Shape [av_r,av_r.....] - length Horizon
     average_rewards = np.mean(random_rewards_all_runs, axis=0)
     average_regrets = np.mean(random_regrets_all_runs, axis=0)
 
-    plot_rew_reg(average_rewards, headline="Random Policy Rewards")
-    plot_rew_reg(average_regrets, headline="Random Policy Regrets")
+    # Do ploting
+    if policy is do_single_run_random_policy:
+        headline = "Random Policy"
+    elif policy is do_single_run_greedy_policy:
+        headline = "Greedy Policy"
+    elif policy is do_single_run_ucb1_policy:
+        pass
+
+    plot_rew_reg(average_rewards, headline=f"{headline} Rewards")
+    plot_rew_reg(average_regrets, headline=f"{headline} Regrets")
 
 
 if __name__ == '__main__':
-    full_plot_run(random_policy)
+    # full_plot_run(do_single_run_random_policy)
+    full_plot_run(do_single_run_greedy_policy)
